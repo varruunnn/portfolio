a1a37573c83020d7d3bfcbee1fe10bbba45b2c9d
varruunnn <varun.choudhary2512@gmail.com>
2024-12-22 17:20:09 +0530

    p

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -17,7 +17,12 @@ const Projects = () => {
                         {" "}
                         Secure your spot, sharpen your skills, and get ready to dominate—because at BigGameWars, every
                         match could makse you a legend.
+                        <br />
+                        NOTE: This is under development but you can check the website ":)".
+                        <br />
                         FOR MORE CHECKOUT READMEfile
+                        <br />
+                        MADE WITH MERN STACK
                       </strong>
                     </p>
 
@@ -27,7 +32,7 @@ const Projects = () => {
         <div className="project-card">
           <h3>Video Player</h3>
           <img src="" alt="" />
-          <p>🚀 Just built a video player app with React & MongoDB! 🎬 Upload, play, and your videos stay saved even after reloading with all videoplayer features (eg double click fullscreen etc). Check it out!</p>
+          <p>🚀 Just built a video player app with MERN & MongoDB! 🎬 Upload, play, and your videos stay saved even after reloading with all videoplayer features (eg double click fullscreen etc). Check it out!</p>
           <a className="links" href="https://github.com/varruunnn/videoPlayer">GITHUB</a>
           <a className="links" href="https://private-user-images.githubusercontent.com/143994029/384676413-0fc14560-7dbe-416e-b7e8-2dd09665d8cf.mp4?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3MzQ4NjUwODMsIm5iZiI6MTczNDg2NDc4MywicGF0aCI6Ii8xNDM5OTQwMjkvMzg0Njc2NDEzLTBmYzE0NTYwLTdkYmUtNDE2ZS1iN2U4LTJkZDA5NjY1ZDhjZi5tcDQ_WC1BbXotQWxnb3JpdGhtPUFXUzQtSE1BQy1TSEEyNTYmWC1BbXotQ3JlZGVudGlhbD1BS0lBVkNPRFlMU0E1M1BRSzRaQSUyRjIwMjQxMjIyJTJGdXMtZWFzdC0xJTJGczMlMkZhd3M0X3JlcXVlc3QmWC1BbXotRGF0ZT0yMDI0MTIyMlQxMDUzMDNaJlgtQW16LUV4cGlyZXM9MzAwJlgtQW16LVNpZ25hdHVyZT02MzdlMDQyMTMzNzgyMWI1YTcwYTU1N2Y3MGMxYjU3ZTdhNzUwMjI3ODIwNTJmM2UzZThlZDMwMmNhOWZhYjk0JlgtQW16LVNpZ25lZEhlYWRlcnM9aG9zdCJ9.NzFUhTicKg2DsLiVhHQfAzZvz8NP7SXtT0hZCb0MDoM">DEMO</a>
         </div>
